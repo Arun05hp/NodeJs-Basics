0a7a99a137b1e9692879fb8c6bb6bc90dfd22c1a
Arun Kumar <kumar05.962013@gmail.com>
2020-08-23 16:22:44 +0530

    callback

---
+++ async/callback.js
@@ -0,0 +1,18 @@
+const users = require("./users.json");
+const movies = require("./movies.json");
+const reviews = require("./reviews.json");
+
+const getMovie = (id, cb) => {
+  setTimeout(() => {
+    const movie = movies.find((movie) => movie.id === id);
+    cb(movie);
+  }, 1000);
+};
+
+const getReview = () => {};
+
+const getUser = () => {};
+
+getMovie(3, (movie) => {
+  console.log(movie);
+});
