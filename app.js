6626a34f5af6bc585f84883e8c37330c7a653e92
Arun Kumar <kumar05.962013@gmail.com>
2020-08-23 15:16:42 +0530

    start and terminate a process

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 const http = require("http");
+const port = 5000;
+
+const dbConnection = true;
+
+if (!dbConnection) process.exit(1);
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      res.end(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <title>NodeJs</title>
+            </head>
+            <body>
+                <h1>Welcome to my First Node App</h1>
+            </body>
+            </html>
+            `);
+      break;
+    case "/admin":
+      res.end(`Welcome to admin pagepage`);
+      break;
+    case "/user":
+      res.end(`Welcome to user pagepage`);
+      break;
+    default:
+      res.end(`Page Not Found`);
+      break;
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is now listening on port ${port}`);
+});
