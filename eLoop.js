5377db277e0aff7911f0ef92043f9bd54b713acc
Arun Kumar <kumar05.962013@gmail.com>
2020-08-23 15:39:33 +0530

    Event loop

--- eLoop.js
+++ eLoop.js
@@ -1,7 +1,22 @@
 const { pbkdf2 } = require("crypto");
+const { createServer } = require("http");
+
 const start = Date.now();
 const hash = () => {
   pbkdf2("pass", "salt", 10000, 1000, "sha256", () => {
     console.log("Hash", Date.now() - start);
   });
 };
+
+const lis = () => {
+  createServer().listen(3000, () => {
+    console.log("listen", Date.now() - start);
+  });
+};
+
+hash();
+hash();
+hash();
+hash();
+hash();
+lis();
